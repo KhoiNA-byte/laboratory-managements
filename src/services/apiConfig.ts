1bf19da9fc415918364c2bd2268e04853dafde87
shadowpro50 <khoinguyenanh205@gmail.com>
2025-11-06 10:03:23 +0700

    integrate role with redux and api

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -1,10 +1,14 @@
-// src/services/apiConfig.ts
-
+// services/apiConfig.ts
 const API_BASE_URL = import.meta.env.VITE_MOCKAPI_BASE_URL;
 
 // User endpoints
 export const USERS_ENDPOINT = `${API_BASE_URL}${
   import.meta.env.VITE_MOCKAPI_USERS_ENDPOINT
+}`;
+
+// Roles endpoints
+export const ROLES_ENDPOINT = `${API_BASE_URL}${
+  import.meta.env.VITE_MOCKAPI_ROLES_ENDPOINT
 }`;
 
 export { API_BASE_URL };
